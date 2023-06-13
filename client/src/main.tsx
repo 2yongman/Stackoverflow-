e6ddcdbf54e1e1b13aa7899c4fae7cf1a4f32761
Jiwonp12 <ndwoo33@gmail.com>
2023-06-13 21:03:02 +0900

    FE setting

---
+++ client/src/main.tsx
@@ -0,0 +1,10 @@
+import { RecoilRoot } from 'recoil';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+);
